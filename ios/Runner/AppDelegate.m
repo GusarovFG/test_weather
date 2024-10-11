8b013a110cf8ef5f0945c635b6952904efb37ef9
GusarovFG <75736828+GusarovFG@users.noreply.github.com>
2024-10-11 18:40:51 +0400

    Update AppDelegate.m

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #import "AppDelegate.h"
 #import "FlutterPluginRegistrant/GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyDKm3jFcnPCcRfpo9CkxPqqidly8ZZ5-6A"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
